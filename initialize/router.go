16a30f63c19ebca581aab906ca21ba275c515bc7
sunnchao <sunchaowang@outlook.com>
2024-04-16 22:59:29 +0800

    Update router.go

--- initialize/router.go
+++ initialize/router.go
@@ -29,11 +29,11 @@ func RegisterRouter() *gin.Engine {
 
 	router.POST("/auth/session", handler.session)
 	router.POST("/auth/refresh", handler.refresh)
-	router.OPTIONS("/v1/chat/completions", optionsHandler)
+	router.OPTIONS("/hf/chat/completions", optionsHandler)
 
 	authGroup := router.Group("").Use(middlewares.Authorization)
-	authGroup.POST("/v1/chat/completions", handler.nightmare)
-	authGroup.GET("/v1/models", handler.engines)
+	authGroup.POST("/hf/chat/completions", handler.nightmare)
+	authGroup.GET("/hf/models", handler.engines)
 	authGroup.POST("/backend-api/conversation", handler.chatgptConversation)
 	return router
 }
